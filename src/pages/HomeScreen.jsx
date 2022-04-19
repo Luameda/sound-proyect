fcbf8296482eb86546f21e0b4236545ae111feb7
CEO Lnx <ceo@luameda.com>
2022-04-19 15:45:00 -0500

    The controls are working

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,161 +1,8 @@
-/* import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TouchableOpacity
-} from 'react-native'
-import { useState, useEffect } from 'react'
-import { FontAwesome5 } from '@expo/vector-icons'
-import { Audio } from 'expo-av'
-
-import { music } from '../../dumData'
-
-export default function HomeScreen() {
-    const [playing, setPlaying] = useState(false)
-    const [playbackObj, setPlaybackObj] = useState(null)
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [volume, setVolume] = useState(1.0)
-    const [buffering, setBuffering] = useState(false)
-
-    const loadAudio = async () => {
-        try {
-            const playbackInstance = new Audio.Sound()
-            const source = { uri: music[currentIndex].url }
-            const status = { shouldPlay: playing, volume }
-
-            playbackInstance.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate)
-            await playbackInstance.loadAsync(source, status, false)
-
-            setPlaybackObj({ playbackInstance })
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    const onPlaybackStatusUpdate = status => {
-        setBuffering(status.buffering)
-    }
-
-    const handlePlayPause = async () => {
-        playing ? await playbackObj.pauseAsync() : playbackObj.playAsync()
-        setPlaying(!playing)
-    }
-
-    const handlePreviousTrack = async () => {
-        if (playbackObj) {
-            await playbackObj.unloadAsync()
-            currentIndex < music.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
-
-            setCurrentIndex({ currentIndex })
-            loadAudio()
-        }
-    }
-
-    const handleNextTrack = async () => {
-        if (playbackObj) {
-            await playbackObj.unloadAsync()
-            currentIndex < music.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
-
-            setCurrentIndex(currentIndex)
-        }
-    }
-
-    useEffect(async () => {
-        try {
-            await Audio.setAudioModeAsync({
-                allowsRecordingIOS: false,
-                interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-                playsInSilentModeIOS: true,
-                interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-                shouldDuckAndroid: true,
-                staysActiveInBackground: true,
-                playThroughEarpieceAndroid: true
-            })
-
-            loadAudio()
-        } catch (err) {
-            console.error(err)
-        }
-
-        return playbackObj ?
-            () => {
-                playbackObj.unloadAsync()
-            }
-            : undefined
-    }, [playbackObj])
-
-
-    return (
-        <View style={styles.container}>
-            <Image
-                style={styles.albumCover}
-                source={{ uri: "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg" }}
-            />
-
-            <View style={styles.controls}>
-                <TouchableOpacity style={styles.control} onPress={handlePreviousTrack} >
-                    <FontAwesome5 name="step-backward" size={48} color="#444" />
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.control} onPress={handlePlayPause} >
-                    {playing
-                        ?
-                        (<FontAwesome5 name='pause' size={48} color='#444' />)
-                        :
-                        (<FontAwesome5 name="play" size={48} color="#444" />)
-                    }
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.control} onPress={handleNextTrack} >
-                    <FontAwesome5 name="step-forward" size={48} color="#444" />
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    albumCover: {
-        width: 250,
-        height: 250
-    },
-    trackInfo: {
-        padding: 40,
-        backgroundColor: '#fff'
-    },
-    trackInfoText: {
-        textAlign: 'center',
-        flexWrap: 'wrap',
-        color: '#550088'
-    },
-    largeText: {
-        fontSize: 22
-    },
-    smallText: {
-        fontSize: 16
-    },
-    controls: {
-        flexDirection: 'row'
-    },
-    control: {
-        margin: 20
-    }
-}) */
-
-
-
 import { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Audio } from 'expo-av';
 import { music } from '../../dumData';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import { Sound } from 'expo-av/build/Audio/Sound'
 
 export default function App() {
@@ -163,6 +10,7 @@ export default function App() {
     const [sound, setSound] = useState(null)
     // const [isPlaying, setIsPlaying] = useState(true)
     const [status, setStatus] = useState(null)
+    const [currentIndex, setCurrentIndex] = useState(0)
 
     const onPlaybackStatusUpdate = status => setStatus(status)
 
@@ -172,7 +20,7 @@ export default function App() {
         }
 
         const { sound: newSound } = await Sound.createAsync(
-            { uri: song[2].url },
+            { uri: song[currentIndex].url },
             { shouldPlay: true },
             onPlaybackStatusUpdate
         )
@@ -203,6 +51,34 @@ export default function App() {
             await sound.playAsync();
         }
     }
+    const handleRepeat = async () => {
+        await sound.replayAsync()
+    }
+
+    const handlePreviousTrack = () => {
+        if (status.positionMillis > status.durationMillis / 6) {
+            handleRepeat()
+        } else {
+            if (currentIndex === 0) {
+                handleRepeat()
+            } else {
+                setCurrentIndex(currentIndex - 1)
+            }
+        }
+    }
+
+    const handleNextTrack = () => {
+        let newIndex;
+
+        if (currentIndex < music.length - 1) {
+            newIndex = currentIndex + 1
+        }
+        else {
+            newIndex = 0
+        }
+
+        setCurrentIndex(newIndex)
+    }
 
 
     useEffect(() => {
@@ -210,24 +86,52 @@ export default function App() {
         setSong(music)
         if (song) {
             playCurrentSong();
-            console.log(status)
+            // console.log(status)
         }
-    }, [song])
+    }, [song, currentIndex])
 
 
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity
-                onPress={onPlayPausePress}
-                children={
-                    <FontAwesome5
-                        name={status?.isPlaying ? 'pause' : 'play'}
-                        size={40}
-                        color='black'
-                    />
+            <View style={styles.controls}>
+
+                <TouchableOpacity style={styles.control} onPress={handlePreviousTrack}>
+                    <FontAwesome5 name='backward' size={40} color='black' />
+                </TouchableOpacity>
+                {
+                    status?.positionMillis === status?.durationMillis ?
+                        (
+                            <TouchableOpacity
+                                onPress={handleRepeat}
+                                children={
+                                    <FontAwesome
+                                        name='repeat'
+                                        size={40}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        ) :
+                        (
+                            <TouchableOpacity
+                                onPress={onPlayPausePress}
+                                style={styles.control}
+                                children={
+                                    <FontAwesome5
+                                        name={status?.isPlaying ? 'pause' : 'play'}
+                                        size={38}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        )
                 }
-            />
+                <TouchableOpacity style={styles.control} onPress={handleNextTrack}>
+                    <FontAwesome5 name='forward' size={40} color='black' />
+                </TouchableOpacity>
+            </View>
+
             <Text>{status?.positionMillis} / {status?.durationMillis}</Text>
             <Text>{status?.didJustFinish && 'Terminado'}</Text>
         </View>
@@ -239,5 +143,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    controls: {
+        flexDirection: 'row'
+    },
+    control: {
+        margin: 8
     }
 })
--- src/pages/SearchScreen.jsx
+++ src/pages/SearchScreen.jsx
@@ -1,11 +1,32 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useEffect } from 'react'
+import { music } from '../../dumData';
+import { FontAwesome } from '@expo/vector-icons';
 
 
 export default function SearchScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text> Search Screen </Text>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            {
+                music.map((track, key = 0) => {
+                    return (
+                        <View key={key++} style={styles.item} >
+                            <TouchableOpacity onPress={() => alert('')}>
+                                <FontAwesome name="play" size={40} color='black' />
+                            </TouchableOpacity>
+                            <Text>{track.title}</Text>
+                            <Text>{track.artits}</Text>
+                        </View>
+                    )
+                })
+            }
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    item: {
+        borderBottomWidth: 1,
+        marginBottom: 7
+    }
+})
